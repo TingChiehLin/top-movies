c63cb14878d42d957a99547dd0cdcd7c7a8a1e4c
TingChiehLin <cooloojayoo@gmail.com>
2023-07-01 22:57:27 +1000

    Add - Styling on Select Form

--- components/FliterBar/FliterBar.tsx
+++ components/FliterBar/FliterBar.tsx
@@ -1,39 +1,30 @@
 "use client";
 
 import * as React from "react";
-import { FiChevronUp } from "react-icons/fi";
+import { FiChevronDown } from "react-icons/fi";
 
 import { FILTER_ITEMS } from "../../lib/filterItems";
 
 const FliterBar = () => {
   const [value, setValue] = React.useState(FILTER_ITEMS[0].value);
-  const [isOpen, setIsOpen] = React.useState(false);
   const ref = React.useRef();
 
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
+  const handleClick = () => {};
 
   const handChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setValue(e.target.value);
   };
-  const handleClickOutside = () => {
-    setIsOpen(false);
-  };
+  const handleClickOutside = () => {};
 
   return (
-    <div className="w-56 flex items-center gap-4 bg-red-300">
-      <label htmlFor="movie-filter">Filter Genre:</label>
-      <FiChevronUp
-        size={24}
-        className={`absolute right-0 top-0 transition ease-out duration-300 ${
-          isOpen && "rotate-180"
-        }`}
-      />
+    <div className="relative w-32 flex items-center gap-4">
+      {/* <label className="w-52" htmlFor="movie-filter">
+        Filter Genre:
+      </label> */}
       <select
         id="movie-select"
         value={value}
-        className="px-3 py-2 bg-sky-700 text-white rounded-lg appearance-none cursor-pointer"
+        className="w-full px-4 py-2 outline-0 bg-sky-700 text-white rounded-lg cursor-pointer appearance-none"
         onClick={handleClick}
         onChange={handChange}
       >
@@ -41,22 +32,20 @@ const FliterBar = () => {
           <option
             key={option.value}
             value={option.value}
-            className="pl-4 py-4 cursor-pointer active:text-white active:bg-sky-700 hover:bg-slate-200"
+            className="px-4 py-4 cursor-pointer active:text-white active:bg-sky-700 hover:bg-slate-200"
             onClick={handleClick}
           >
             {option.label}
           </option>
         ))}
       </select>
+      <FiChevronDown
+        size={16}
+        className={"absolute right-3 top-1/2 -translate-y-1/2"}
+        color="white"
+      />
     </div>
   );
 };
 
 export default FliterBar;
-
-{/* <FiChevronUp
-size={24}
-className={`transition ease-out duration-300 ${
-  isOpen && "rotate-180"
-}`}
-/> */}
